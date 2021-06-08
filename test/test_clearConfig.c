218ccc45a1d61e8c49a83e52873af975ccc66602
FanatiQS <fanatiqs.com@gmail.com>
2021-06-08 15:06:13 +0200

    Added tool to clear eeprom config data

---
+++ test/test_clearConfig.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "../../verbalEyes_speed_controller.h"
+
+#include "./eeprom.h"
+#include "./log.h"
+
+
+
+// Only defined to not throw compilation errors
+void verbaleyes_network_connect(const char* ssid, const char* key) {}
+bool verbaleyes_network_connected() { return 0; }
+uint32_t verbaleyes_network_getip() { return 0; }
+void verbaleyes_socket_connect(const char* host, const unsigned short port) {}
+bool verbaleyes_socket_connected() { return 0; }
+short verbaleyes_socket_read() { return 0; }
+void verbaleyes_socket_write(const uint8_t* str, const size_t len) {}
+unsigned char verbaleyes_conf_read(const unsigned short addr) { return 0; }
+
+
+
+int main() {
+	// Sets eeprom to be full of data
+	memset(conf_eeprom, 1, CONFIGLEN);
+
+	// Runs through clearConfig script data
+	char c;
+	while ((c = getc(stdin)) != EOF) {
+		updateConfig(c);
+	}
+
+	// Verifies it cleared entire EEPROM
+	for (int i = 0; i < CONFIGLEN; i++) {
+		if (conf_eeprom[i] != 0) {
+			for (int i = 0; i < CONFIGLEN; i++) {
+				printf("%i\t%i\n", i, conf_eeprom[i]);
+			}
+			return 0;
+		}
+	}
+
+	printf("\n\x1b[32mTest successful: Entire EEPROM can be cleared correctly\n\n\x1b[0m");
+
+	return 0;
+}
---
+++ tools/clearConfig.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# The first argument is a path to where to output the configuration commands or to serial if undefined
+# The second argument prevents delaying writes as done by default to not risk loosing data in transmission
+
+delay=$2
+
+if [[ $# > 0 ]]; then
+	path=$1
+elif [ $(uname) == "Darwin" ]; then
+	path=$(ls /dev/cu.usbserial-*)
+else
+	path=$(/dev/ttyUSB*)
+fi
+
+if [ -z "$path" ]; then
+	echo "Unable to get serial interface"
+	exit
+fi
+
+clearString() {
+	key=$1
+	len=$2
+	while [[ $len > 0 ]]
+	do
+		((len--))
+		printf $key= >> $path
+		for ((i = 0; i < len; i++)) {
+			printf x >> $path
+		}
+		if [[ -z "$delay" ]]
+		then
+			sleep 0.1
+		fi
+		printf "\\n" >> $path
+	done
+}
+
+printf "" > $path
+
+clearString ssid 32
+clearString ssidkey 63
+clearString host 64
+clearString path 32
+clearString proj 32
+clearString projkey 32
+
+printf "port=0\\n" >> $path
+printf "speedmin=0\\n" >> $path
+printf "speedmax=0\\n" >> $path
+printf "deadzone=0\\n" >> $path
+printf "callow=0\\n" >> $path
+printf "calhigh=0\\n" >> $path
+printf "sensitivity=0\\n" >> $path
+
+printf "\\n" >> $path
